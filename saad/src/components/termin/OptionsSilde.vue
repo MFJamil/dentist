c93919379d18d1c764385c7eca509f0507f909e9
Mohammed Jamil <mohammed.jamil@smyld.org>
2021-11-06 23:39:19 +0100

    Update

--- saad/src/components/termin/OptionsSilde.vue
+++ saad/src/components/termin/OptionsSilde.vue
@@ -38,17 +38,19 @@
                 </v-row>
             </div>
             <div v-else style="width:100%;">
-                  <v-list style="width:100%;">
+                  <v-list style="width:100%;" @touchstart="touchStart" @touchmove="touchMove">
                     <template v-for="opt in opts" >
                         <v-list-item 
                             :key="opt.name"
+                            :style="'border-radius:10px;background-color:' + (selectedOpt!=opt.name?'white':'var(--v-primary-base)')"
                         >
                         <v-list-item-avatar size="50" >
                             <v-img  :src="opt.pic"></v-img>
                         </v-list-item-avatar>
 
                         <v-list-item-content :style="lang=='ar'?'text-align:right;':'text-align:left;'">
-                            <v-list-item-title v-html="$i18n.t(opt.name)" @click="handleClick(opt)"></v-list-item-title>
+                            <v-list-item-title class="treatmentTitle" :style="'color:' + (selectedOpt===opt.name?'white':'var(--v-primary-base)')" 
+                             v-html="$i18n.t(opt.name)" @click="handleClick(opt)"></v-list-item-title>
                             <v-list-item-subtitle v-html="opt.subtitle"></v-list-item-subtitle>
                         </v-list-item-content>
                         </v-list-item>
@@ -83,6 +85,8 @@ export default class OptionsSlide extends Vue {
   @Prop()
   public updateField!:any;
 
+  public xDown:any=null;
+  public yDown:any=null;
 
 
   public selectedOpt='';
@@ -94,6 +98,43 @@ export default class OptionsSlide extends Vue {
     this.$emit('done', 1);
   }
 
+   touchStart(e:any){
+       //console.log("Touch Start detected ....");
+        const firstTouch = e.touches[0];                                      
+        this.xDown = firstTouch.clientX;                                      
+        this.yDown = firstTouch.clientY;          
+   }
+   touchMove(e:any){
+       //console.log("Touch Move detected ....");
+       if (this.yDown==null||this.xDown==null) return;
+        let xUp = e.touches[0].clientX;                                    
+        let yUp = e.touches[0].clientY;
+
+        let xDiff = this.xDown - xUp;
+        let yDiff = this.yDown - yUp;
+         if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+            if ( xDiff > 0 ) {
+                /** Touch was to the right */
+            } else {
+                /** Touch was to the right */
+                    
+            }                       
+        } else {
+            if ( yDiff > 0 ) {
+                /** swipe down */
+                this.$emit('done', 1);
+                
+            } else { 
+                /* up swipe */
+                this.$emit('cancel', 1);
+                
+            }                                                                 
+        }
+        /* reset values */
+        this.xDown = null;
+        this.yDown = null;  
+        e.preventDefault();                                          
+   }
 
 
 }
